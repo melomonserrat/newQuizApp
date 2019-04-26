35f141c0f2b5f57cb6fce1b3a331f3679e36cc75
Melo Monserrat <melo.monserrat@gmail.com>
2019-04-26 22:34:34 +0800

    manageCourses frontend

--- quizTaker.php
+++ quizTaker.php
@@ -46,7 +46,7 @@
 	<?php
 		session_start();
 		echo $_SESSION['username'];
-		$mysqli = new mysqli( 'localhost', 'root', '', 'newquizapp');
+		$mysqli = new mysqli( 'localhost', 'root', '', 'quizapp');
 		if($mysqli->connect_error){
 			die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
 		}else{

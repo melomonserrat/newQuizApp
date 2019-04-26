ca8b5ef7e2b040face9b4be31a60db3f9588294d
Melo Monserrat <melo.monserrat@gmail.com>
2019-04-27 03:10:42 +0800

    hello

--- quizTaker.php
+++ quizTaker.php
@@ -12,7 +12,10 @@
     <title>Quiz App</title>
   
   </head>
-  <body>
+  <body style="background-image: url('pic5.jpg');">
+	<?php
+		session_start();
+	?>
 	<nav class="navbar navbar-expand-lg navbar-light bg-light">
 		<a class="navbar-brand" href="welcome.php">Quiz App</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,33 +23,31 @@
 		</button>
 		<div class="collapse navbar-collapse" id="navbarNavDropdown">
 			<ul class="navbar-nav">
-				<li class="nav-item active">
-					<a class="nav-link" href="welcome.php">Home<span class="sr-only">(current)</span></a>
-				</li>
 				<li class="nav-item">	
 					<div class="dropdown">
-						<button class="btn btn-outline-dark dropdown-toggle" type="button" id="manageCoursesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<button class="btn btn-outline-dark dropdown-toggle" type="button" id="manageCoursesDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" style="color:white; text-shadow: 2px 2px 8px #000000;">
 							Manage Courses
 						</button>
 
 						<div class="dropdown-menu" aria-labelledby="manageCoursesDropdown">
-							<a class="dropdown-item" href="javascript:manageCourse()">Manage a course</a>
+							<a class="dropdown-item" href="manageCourses.php?create">Create a course</a>
+							<a class="dropdown-item" href="manageCourses.php?edit">Edit a course</a>
+							<a class="dropdown-item" href="manageCourses.php?view">View courses</a>
 						</div>
 					</div>
 				</li>
 			</ul>
 		</div>
-		
+		<li class="loggedIn">
+			<p class="loggedIn">Logged in as <?php echo ("{$_SESSION['username']}" . " ");  ?> </p>
+		</li>	
 		<form class="form-inline" action="quizmaker.php">
 			<input type="hidden" name="form" value="logout">
-			<button class="btn btn-outline-secondary my-2 my-sm-0" type="submit">Logout</button>
+			<button class="btn btn-dark my-2 my-sm-0" type="submit" style="text-shadow: 2px 2px 8px #000000;">Logout</button>
 		</form>
 	</nav>
 	<div class="card-columns" align="center">
 	<?php
-		session_start();
-
-		echo $_SESSION['username'];
 		$mysqli = new mysqli( 'localhost', 'root', '', 'quizapp');
 		if($mysqli->connect_error){
 			die( 'Connect Error: ' . $mysqli->connect_errno . ': ' . $mysqli->connect_error);
@@ -63,25 +64,27 @@
 			    	$sql1 = "SELECT * FROM quiz WHERE Course_ID = ". $record['Course_ID'];
 			    	$quizCourseID = $mysqli->query($sql1);
 			    	$count = $count +1;
-			    	?>	
-					<div class="card">
-					<div class="card-body text-center">
+	?>	
+
+			<div class="card text-white bg-dark mb-3" style="margin-top:100px; margin-left:400px;">
+				<div class="card-body text-center">
 					<div class="title">
 						<h4><?php echo $record['Course_Name']; ?></h4>
 					</div>
 					<div class="desc"><?php echo $record['Course_Description']; ?></div>
-					</div>
+				</div>
 					<button type="button" class="btn btn-primary" data-toggle="modal" <?php echo "data-target='#myModal".$count."'" ?> >View Quiz List</button>
-					</div>
+			</div>
 
-					<div class="modal" <?php echo "id='myModal". $count."'" ?> >
-					  <div class="modal-dialog modal-lg">
-					    <div class="modal-content">
 
-					      <!-- Modal Header -->
-					      <div class="modal-header">
-					        <h4 class="modal-title"><?php echo $record['Course_Name']; ?></h4>
-					        <button type="button" class="close" data-dismiss="modal">&times;</button>
+		<div class="modal" <?php echo "id='myModal". $count."'" ?> >
+			<div class="modal-dialog modal-lg">
+				<div class="modal-content">
+
+				 <!-- Modal Header -->
+					<div class="modal-header">
+					    <h4 class="modal-title"><?php echo $record['Course_Name']; ?></h4>
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
 					      </div>
 
 					      <!-- Modal body -->

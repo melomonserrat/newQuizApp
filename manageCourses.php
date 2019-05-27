0c4da0643a4183cc7d6ed0e0eb6ae8e55a07e31a
Nathan Cabreza <nathan.cabreza1@gmail.com>
2019-05-27 09:14:47 +0800

    Changed jquery versions.

--- manageCourses.php
+++ manageCourses.php
@@ -164,11 +164,12 @@
     <div class="container viewCourses" style="text-shadow: 4px 4px 8px #000000;">
         <h4>Viewing all courses... </h4>
         <hr>
-        <table class="table" style="color: white;">
+        <table class="table courseTable" style="color: white;">
             <thead>
                 <tr>
                     <th scope="col">Course Name</th>
                     <th scope="col">Course Description</th>
+                    <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -180,12 +181,13 @@
                         die();
                     }
 
-                    $result = mysqli_query($con, "SELECT course_name, course_description FROM course WHERE course_isopen = 1");
+                    $result = mysqli_query($con, "SELECT course_name, course_description, course_id FROM course WHERE course_isopen = 1");
 
                     while($row = mysqli_fetch_array($result)){
-                        echo "<tr>";
+                        echo "<tr id=\"" . $row['course_id'] . "\">";
                         echo "<td>" . $row['course_name'] . "</td>";
                         echo "<td>" . $row['course_description'] . "</td>";
+                        echo "<td><button type=\"button\" class=\"btn btn-primary viewCourseButton\" onclick=\"viewCourse(this)\">View</button></td>";
                         echo "</tr>";
                     }
                 ?>
@@ -247,7 +249,7 @@
 	?>
     
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="initManageCourses.js"></script>
--- manageQuizzes.php
+++ manageQuizzes.php
@@ -397,7 +397,7 @@
                         die();
                     }
 
-                    $result = mysqli_query($con, "SELECT quiz_name, quiz_description, quiz.quiz_difficulty FROM quiz,course WHERE quiz.Course_ID = course.Course_ID AND course.Course_isOpen = 1");
+                    $result = mysqli_query($con, "SELECT quiz_name, quiz_description, quiz.quiz_difficulty FROM quiz,course WHERE quiz.Course_ID = course.Course_ID AND course.Course_isOpen = 1 AND quiz.is_open = 1");
 
                     while($row = mysqli_fetch_array($result)){
                         echo "<tr>";
@@ -424,7 +424,7 @@
 	?>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="initManageQuizzes.js"></script>
